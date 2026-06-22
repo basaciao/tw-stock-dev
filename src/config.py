9d24007455ff09e816a6fd9e9cffe6c98a91746b
basaciao <basahsieh@gmail.com>
2026-06-22 15:38:05 +0800

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,33 @@
+import os
+
+# ===========================
+# Telegram
+# ===========================
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+# ===========================
+# Update Time
+# ===========================
+
+UPDATE_HOUR = 15
+UPDATE_MINUTE = 30
+
+# ===========================
+# Taiwan Stock API
+# ===========================
+
+TWSE_URL = "https://www.twse.com.tw"
+
+# ===========================
+# Data Folder
+# ===========================
+
+DATA_FOLDER = "data"
+
+# ===========================
+# Report Setting
+# ===========================
+
+TOP_N = 10
