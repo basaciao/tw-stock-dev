885910f15c09461a7c819db23bfb2359ad65a65f
basaciao <basahsieh@gmail.com>
2026-06-22 15:57:06 +0800

    Create twse.py

---
+++ src/crawler/twse.py
@@ -0,0 +1,22 @@
+import requests
+
+from config import TWSE_URL
+
+
+def get_twse_daily_data(date: str):
+
+    url = f"{TWSE_URL}/exchangeReport/STOCK_DAY_ALL"
+
+    params = {
+        "response": "json",
+        "date": date.replace("-", "")
+    }
+
+    res = requests.get(url, params=params)
+
+    if res.status_code != 200:
+        raise Exception("TWSE API error")
+
+    data = res.json()
+
+    return data
