9353a7037df968859ea257a7fb85de83e11e7753
basaciao <basahsieh@gmail.com>
2026-06-23 13:34:38 +0800

    Update engine.py

--- src/technical/engine.py
+++ src/technical/engine.py
@@ -1,10 +1,11 @@
+from models.stock_data import StockData
 from technical.rsi import calculate_rsi
 
 
-def calculate_all_indicators(stock_df):
+def calculate_all_indicators(stock: StockData):
 
     result = {}
 
-    result["RSI"] = calculate_rsi(stock_df)
+    result["RSI"] = calculate_rsi(stock)
 
     return result
